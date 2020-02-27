df195911a7242dbb2f2b1b003eea20c66d2ccdb9
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-02-27 12:44:02 +0530

    reset button implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:swipedetector/swipedetector.dart';
 import 'package:flutter/material.dart';
 import 'game_logic.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -26,86 +27,111 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<List<int>> matrix=initializeMatrix();
-  String s='';
-  bool adjacentCellsMatch(matrix){
-    int i,j;
-    bool match=false;
-    for(i=0;i<matrix.length;i++){
-      for(j=0;j<matrix[i].length;j++){
-        match=match|| checkLeft(matrix,i,j)||checkRight(matrix,i,j)||checkUp(matrix,i,j)||checkDown(matrix,i,j);
+  List<List<int>> matrix = initializeMatrix();
+  String s = '';
+
+  bool adjacentCellsMatch(matrix) {
+    int i, j;
+    bool match = false;
+    for (i = 0; i < matrix.length; i++) {
+      for (j = 0; j < matrix[i].length; j++) {
+        match = match ||
+            checkLeft(matrix, i, j) ||
+            checkRight(matrix, i, j) ||
+            checkUp(matrix, i, j) ||
+            checkDown(matrix, i, j);
       }
     }
     return match;
   }
+
 //  bool adjacentCellsMatch(matrix){
 //    matrix.forEach((e)=>e.forEach((n)=>checkLeft(matrix,e[0],e[1])||checkRight(matrix,e[0],e[1])||checkUp(matrix,e[0],e[1])||checkDown(matrix,e[0],e[1])));
 //  }
-  bool checkLeft(matrix,i,j){
-    return j==0?false:matrix[i][j-1]==matrix[i][j];
+  bool checkLeft(matrix, i, j) {
+    return j == 0 ? false : matrix[i][j - 1] == matrix[i][j];
   }
-  bool checkRight(matrix,i,j){
-    return j==3? false:matrix[i][j+1]==matrix[i][j];
+
+  bool checkRight(matrix, i, j) {
+    return j == 3 ? false : matrix[i][j + 1] == matrix[i][j];
   }
-  bool checkUp(matrix,i,j){
-    return i==0?false:matrix[i-1][j]==matrix[i][j];
+
+  bool checkUp(matrix, i, j) {
+    return i == 0 ? false : matrix[i - 1][j] == matrix[i][j];
   }
-  bool checkDown(matrix,i,j){
-    return i==3?false:matrix[i+1][j]==matrix[i][j];
+
+  bool checkDown(matrix, i, j) {
+    return i == 3 ? false : matrix[i + 1][j] == matrix[i][j];
   }
+
   void checkWinGameOver() {
-    if(containsNumber(2048, matrix)){
-      s='You Win!!';
-    }
-    else{
-      if(containsNumber(0, matrix)){
-        matrix=add2ToEmptySpace(matrix);
-      }
-      else{// if 0 is not present check for adjacent matching
-        if(!adjacentCellsMatch(matrix)){
-          s='Game Over';
+    if (containsNumber(2048, matrix)) {
+      s = 'You Win!!';
+    } else {
+      if (containsNumber(0, matrix)) {
+        matrix = add2ToEmptySpace(matrix);
+      } else {
+        // if 0 is not present check for adjacent matching
+        if (!adjacentCellsMatch(matrix)) {
+          s = 'Game Over';
         }
       }
     }
     setState(() {});
   }
-  Container buildIcon(function,icon,s) {
+
+  Container buildIcon(function, icon, s) {
     return Container(
       child: IconButton(
         icon: Icon(icon),
         iconSize: 50,
         color: Colors.blue,
         onPressed: () {
-          matrix=function(matrix,s);
+          matrix = function(matrix, s);
           checkWinGameOver();
-          },
+        },
       ),
       width: 80,
       height: 80,
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              matrix = initializeMatrix();
+              setState(() {
+
+              });
+            },
+          ),
+        ],
       ),
       body: Center(
         child: GestureDetector(
-            onHorizontalDragEnd: (DragEndDetails details){
-              details.primaryVelocity>0?matrix=leftRightSlideMatrix(matrix,'l'):matrix=leftRightSlideMatrix(matrix,'r');
-                matrix=add2ToEmptySpace(matrix);
-                checkWinGameOver();
-                //setState(() {});
-              },
-            onVerticalDragEnd: (DragEndDetails details){
-              details.primaryVelocity>0?matrix=upDownSlideMatrix(matrix,'u'):matrix=upDownSlideMatrix(matrix,'d');
-              matrix=add2ToEmptySpace(matrix);
-              checkWinGameOver();
-             // setState(() {});
-            },
-          child:
-          Container(
+          onHorizontalDragEnd: (DragEndDetails details) {
+            details.primaryVelocity > 0
+                ? matrix = leftRightSlideMatrix(matrix, 'l')
+                : matrix = leftRightSlideMatrix(matrix, 'r');
+            matrix = add2ToEmptySpace(matrix);
+            checkWinGameOver();
+            //setState(() {});
+          },
+          onVerticalDragEnd: (DragEndDetails details) {
+            details.primaryVelocity > 0
+                ? matrix = upDownSlideMatrix(matrix, 'u')
+                : matrix = upDownSlideMatrix(matrix, 'd');
+            matrix = add2ToEmptySpace(matrix);
+            checkWinGameOver();
+            // setState(() {});
+          },
+          child: Container(
             color: Colors.white,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -117,10 +143,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                    buildIcon(leftRightSlideMatrix, Icons.arrow_left,'l'),
-                    buildIcon(upDownSlideMatrix, Icons.arrow_upward,'u'),
-                    buildIcon(upDownSlideMatrix, Icons.arrow_downward,'d'),
-                    buildIcon(leftRightSlideMatrix, Icons.arrow_right,'r'),
+                    buildIcon(leftRightSlideMatrix, Icons.arrow_left, 'l'),
+                    buildIcon(upDownSlideMatrix, Icons.arrow_upward, 'u'),
+                    buildIcon(upDownSlideMatrix, Icons.arrow_downward, 'd'),
+                    buildIcon(leftRightSlideMatrix, Icons.arrow_right, 'r'),
                   ],
                 ),
                 Row(
@@ -129,24 +155,19 @@ class _MyHomePageState extends State<MyHomePage> {
                     RichText(
                       text: TextSpan(
                         text: s,
-                        style: DefaultTextStyle
-                            .of(context)
-                            .style,
-
+                        style: DefaultTextStyle.of(context).style,
                       ),
                     )
                   ],
                 )
               ],
             ),
-          ),),
+          ),
+        ),
       ),
     );
-
   }
-
 }
-
 
 class RowGrid extends StatelessWidget {
   const RowGrid({
@@ -156,11 +177,12 @@ class RowGrid extends StatelessWidget {
   }) : super(key: key);
   final List<List<int>> matrix;
   final int rowNumber;
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: matrix[rowNumber].map((item)=> Cell(element:item)).toList(),
+      children: matrix[rowNumber].map((item) => Cell(element: item)).toList(),
     );
   }
 }
