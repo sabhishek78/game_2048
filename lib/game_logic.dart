4effd8c94f848bd66161b87654d5cf8b44cb8b5c
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-03-22 18:04:19 +0530

    game controller class implemented. dart game working fine.

--- lib/game_logic.dart
+++ lib/game_logic.dart
@@ -10,167 +10,163 @@
 // Game over - When all tiles (cells) are filled and no 2048 then game over
 import 'dart:io';
 import 'dart:math' show Random;
-import 'main.dart';
+
 
 class GameController{
- List<List<int>>matrix;
- String status;
- int moves;
- GameController(){
-   this.matrix=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-   this.status='';
-   this.moves=0;
-   add2ToEmptySpace();
- }
-  resetGame(){
-    matrix=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-    add2ToEmptySpace();
-    status='';
-    moves=0;
- }
-  add2ToEmptySpace(){
-   var row;
-   var col;
-   do{
-     var random = new Random(); // can get a seed as a parameter
+  List<List<int>> matrix;
+  List<List<int>>previousMatrix;
 
-     // Integer between 0 and 100 (0 can be 100 not)
-     row = random.nextInt(matrix.length);
-     col = random.nextInt(matrix.length);
+  List<List<int>> initializeMatrix(){
+    List<List<int>> matrix=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+    matrix=add2ToEmptySpace(matrix);
+    return matrix;
+  }
 
-   }
-   while(matrix[row][col]!=0);
-   matrix[row][col]=2;
-
- }
-
- bool checkLeft(i, j) {
-   return j == 0 ? false : matrix[i][j - 1] == matrix[i][j];
- }
-
- bool checkRight(i, j) {
-   return j == 3 ? false : matrix[i][j + 1] == matrix[i][j];
- }
-
- bool checkUp(i, j) {
-   return i == 0 ? false : matrix[i - 1][j] == matrix[i][j];
- }
-
- bool checkDown(i, j) {
-   return i == 3 ? false : matrix[i + 1][j] == matrix[i][j];
- }
-
- void checkWinGameOver() {
-   if (containsNumber(2048)) {
-     status = 'You Win!!';
-   } else {
-     if (containsNumber(0,)) {
-       add2ToEmptySpace();
-     } else {
-       // if 0 is not present check for adjacent matching
-       if (!adjacentCellsMatch()) {
-         status = 'Game Over';
-       }
-     }
-   }
-  // setState(() {});
- }
- bool containsNumber(int number){
-   for(var i=0;i<matrix.length;i++){
-     for(var j=0;j<matrix.length;j++){
-       if(matrix[i][j]==number){
-         return true;
-       }
-     }}
-   return false;
- }
- List<List<int>> downSlideMatrix() => matrix = upDownSlideMatrix(rightSlide);
-
- List<List<int>> upSlideMatrix() => matrix = upDownSlideMatrix(leftSlide);
-
- List<List<int>> rightSlideMatrix() => matrix = leftRightSlideMatrix(rightSlide);
-
- List<List<int>> leftSlideMatrix() => matrix = leftRightSlideMatrix(leftSlide);
-
- List<List<int>> transpose() {
-
-   List<List<int>> temp=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-
-   for (int i = 0; i < 4; i++) {
-     for (int j = 0; j < 4; j++) {
-       temp[j][i] = matrix[i][j];
-     }
-   }
-   return temp;
- }
- List<int> rightSlide(List<int>tiles) {
-   tiles=tiles.reversed.toList();
-   tiles=leftSlide(tiles);
-   return tiles.reversed.toList();
- }
- List<int> leftSlide(List<int>tiles) {
-   tiles = slideZeros(tiles);
-   for (var j = 0; j < tiles.length - 1; j++) {
-     tiles = slideZeros(tiles);
-     if (tiles[j + 1] == tiles[j]) {
-       //adjacent tiles matching so perform merge
-       tiles[j] = tiles[j] + tiles[j + 1]; //add
-       tiles[j + 1] = 0; //create empty space
-     }
-   }
-   return tiles;
- }
-
- List<List<int>> leftRightSlideMatrix(callback) {
-   for(var i=0;i<matrix.length;i++){
-     matrix[i]=callback(matrix[i]);
-   }
-   moves=moves+1;
-   print('moves'+moves.toString());
-   return matrix;
- }
-
- List<int>  slideZeros(List<int>tiles) {
-   List<int> result = [];
-   for (var i = 0; i < tiles.length; i++) {
-     if (tiles[i] != 0) {
-       result.add(tiles[i]);
-     }
-   }
-   var zeroCount = tiles.length - result.length;
-   // add zeros
-   for (var i = 0; i < zeroCount; i++) {
-     result.add(0);
-   }
-   return result;
- }
- List<List<int>>  upDownSlideMatrix(callback){
-   transpose();
-   for(var i=0;i<matrix.length;i++){
-     matrix[i]=callback(matrix[i]);
-   }
-   moves++;
-   matrix=transpose();
-   return matrix;
- }
+  List<List<int>> add2ToEmptySpace(List<List<int>> matrix){
+    var row;
+    var col;
+    do{
+      var rng = new Random();
+      row =rng.nextInt(matrix.length);
+      col= rng.nextInt(matrix.length);
+    }
+    while(matrix[row][col]!=0);
+    matrix[row][col]=2;
+    return matrix;
+  }
+  bool containsNumber(number,matrix){
+    for(var i=0;i<matrix.length;i++){
+      for(var j=0;j<matrix.length;j++){
+        if(matrix[i][j]==number){
+          return true;
+        }
+      }}
+    return false;
+  }
+  List<List<int>> transpose(List<List<int>> matrix) {
+    List<List<int>> temp=initializeMatrix();
+    for(var i=0;i<matrix.length;i++){
+      for(var j=0;j<matrix.length;j++){
+        temp[i][j]=matrix[j][i];
+      }
+    }
+    return temp;
+  }
+  void printMatrix(List<List<int>> matrix){
+    for(var i=0;i<matrix.length;i++){
+      var str='';
+      for(var j=0;j<matrix.length;j++){
+        str=str+'['+matrix[i][j].toString()+']';
+      }
+      print(str);
+    }
+  }
+  List<List<int>> leftSlideMatrix(List<List<int>> matrix) {
+    for(var i=0;i<matrix.length;i++){
+      matrix[i]=leftSlide(matrix[i]);
+    }
+    return matrix;
+  }
+  List<List<int>> rightSlideMatrix(List<List<int>>matrix) {
+    for(var i=0;i<matrix.length;i++){
+      matrix[i]=rightSlide(matrix[i]);
+    }
+    return matrix;
+  }
+  List<List<int>> upSlideMatrix(List<List<int>>matrix){
+    matrix=transpose(matrix);
+    for(var i=0;i<matrix.length;i++){
+      matrix[i]=leftSlide(matrix[i]);
+    }
+    matrix=transpose(matrix);
+    return matrix;
+  }
+  List<List<int>> downSlideMatrix(List<List<int>>matrix){
+    matrix=transpose(matrix);
+    for(var i=0;i<matrix.length;i++){
+      matrix[i]=rightSlide(matrix[i]);
+    }
+    matrix=transpose(matrix);
+    return matrix;
+  }
 
 
- bool adjacentCellsMatch() {
-   int i, j;
-   bool match = false;
-   for (i = 0; i < matrix.length; i++) {
-     for (j = 0; j < matrix[i].length; j++) {
-       match = match ||
-           checkLeft(i, j) ||
-           checkRight(i, j) ||
-           checkUp(i, j) ||
-           checkDown( i, j);
-       if(match){
-         return true;
-       }
-     }
-   }
-   return match;
- }
+  List<int> rightSlide(tiles) {
+    tiles=tiles.reversed.toList();
+    tiles=leftSlide(tiles);
+    return tiles.reversed.toList();
+  }
+  List<int> leftSlide(tiles) {
+    tiles = slideZeros(tiles);
+    for (var j = 0; j < tiles.length - 1; j++) {
+      tiles = slideZeros(tiles);
+      if (tiles[j + 1] == tiles[j]) {
+        //adjacent tiles matching so perform merge
+        tiles[j] = tiles[j] + tiles[j + 1]; //add
+        tiles[j + 1] = 0; //create empty space
+      }
+    }
+    return tiles;
+  }
+
+  List<int> slideZeros(tiles) {
+    List<int> result = [];
+    for (var i = 0; i < tiles.length; i++) {
+      if (tiles[i] != 0) {
+        result.add(tiles[i]);
+      }
+    }
+    var zeroCount = tiles.length - result.length;
+    for (var i = 0; i < zeroCount; i++) {
+      result.add(0);
+    }
+    return result;
+  }
 }
+void main(){
+  GameController newGame= GameController();
+ newGame.matrix=newGame.initializeMatrix();
+  newGame.printMatrix(newGame.matrix);
+  var moveCount=0;
+  while(newGame.containsNumber(0,newGame.matrix)){
+    moveCount++;
+    print('This is the $moveCount th move');
+    print("Enter l for left .Enter r for right. Enter u for up. Enter d for down");
+    var move=stdin.readLineSync();
+    switch(move){
+      case 'l': print("Making left move");
+      newGame.matrix=newGame.leftSlideMatrix(newGame.matrix);
+      break;
+      case 'r': print("Making right move");
+      newGame.matrix=newGame.rightSlideMatrix(newGame.matrix);
+      break;
+      case 'u': print("Making Up move");
+      newGame.matrix=newGame.upSlideMatrix(newGame.matrix);
+      break;
+      case 'd': print("Making Down move");
+      newGame.matrix=newGame.downSlideMatrix(newGame.matrix);
+      break;
+    }
+    newGame.matrix=newGame.add2ToEmptySpace(newGame.matrix);
+    newGame.printMatrix(newGame.matrix);
+    if(newGame.containsNumber(2048,newGame.matrix)){
+      print("You Win!!"+"Number of Moves=$moveCount");
+    }
+  }
+  if(!newGame.containsNumber(2048,newGame.matrix)){
+    print("Game Over!!"+"Number of Moves=$moveCount");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
